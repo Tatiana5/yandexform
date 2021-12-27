83d47d3a407fdd9513ad24b0ec4eb96e1f89102c
Tatiana5 <shehovtzova.tanya@yandex.ru>
2021-12-27 08:31:24 +0300

    Update info_acp_yandexform.php

--- language/en/info_acp_yandexform.php
+++ language/en/info_acp_yandexform.php
@@ -24,7 +24,7 @@ $lang = array_merge($lang, [
 	'ACP_YANDEXFORM'                       => 'YooMoney form',
 	'ACP_YANDEXFORM_EXPLAIN'               => 'YooMoney form settings',
 	'ACP_YANDEXFORM_TITLE'                 => 'YooMoney form',
-	//'ACP_YANDEXFORM_TITLE_EXPLAIN'         => '',
+	'ACP_YANDEXFORM_TITLE_EXPLAIN'         => '',
 	'ACP_YANDEXFORM_RECEIVER'              => 'Wallet number',
 	'ACP_YANDEXFORM_TARGET'                => 'Reason for collecting money',
 ]);
